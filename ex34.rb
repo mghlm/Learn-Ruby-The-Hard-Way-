3d99bc9a2c29c4cfab446ef31eab4dc82c2e450c
Magnus Holm <mail@magnusholm.com>
2017-02-23 17:04:19 +0000

    finished exercise 34

---
+++ ex34.rb
@@ -0,0 +1,19 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "The animal at 1 is ruby."
+puts "The third (3rd) animal is peacock."
+puts "The first (1st) animal bear."
+puts "The animal at 3 is kangaroo."
+puts "The fifth (5th) animal is whale."
+puts "The animal at 2 is peacock."
+puts "The sixth (6th) animal is platypus."
+puts "The animal at 4 is whale."
+
+puts animal[1]
+puts animal[2]
+puts animal[0]
+puts animal[3]
+puts animal[4]
+puts animal[2]
+puts animl[5]
+puts animal[4]
